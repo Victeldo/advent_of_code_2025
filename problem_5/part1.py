ec0424ac661b5bcbbc7e0b04f5752da44ec8101b
Victeldo <44360797+Victeldo@users.noreply.github.com>
2025-12-21 19:32:01 -0800

    basic brute force solution +cleaned up

--- problem_5/part1.py
+++ problem_5/part1.py
@@ -1,17 +1,7 @@
-### input is a list of pairs of numbers, each pair is a range of numbers. then theres a blank line and then a list of numbers.
-### we need to determine how many of the numbers in the second list are in any of the ranges in the first list.
-
-
-### EDIT: This is an inefficient solution since the set operations are O(L) time complexity where L is the largest number in the list of ranges.
-
-from typing import Any
-
-
-input = open('inputs/input.txt', 'r').read()
 
 def format_input(input):
     input = input.split('\n')
-    ranges = []
+    fresh_ranges = []
     ingredient_time = False
     ingredients = []
 
@@ -20,32 +10,28 @@ def format_input(input):
             ingredient_time = True
             continue
         elif not ingredient_time:
-            ranges.append(line.split('-'))
+            fresh_ranges.append(tuple(map(int, line.split('-'))))
         else:
             ingredients.append(int(line))
     
-    return ranges, ingredients
+    return fresh_ranges, ingredients
 
-
-fresh_ranges, ingredients = format_input(input)
-
-### want to make a set of the ranges, then for each ingredient, check if it is in any of the ranges
-
-def fresh_list(fresh_ranges: list[tuple[str, str]]) -> set[int]:
-    fresh_list = set()
+def is_fresh(fresh_ranges: list[tuple[str, str]], ingredient: int) -> set[int]:
     for range_id in fresh_ranges:
-        items = list(range(int(range_id[0]), int(range_id[1]) + 1))
-        fresh_list.update(items)
-    return fresh_list
-
+        if range_id[0] <= ingredient <= range_id[1]:
+            return True
+    return False
 
 
 def count_fresh_ingredients(ingredients: list[int], fresh_list: set[int]) -> int:
     count = 0
     for ingredient in ingredients:
-        if ingredient in fresh_list:
+        if is_fresh(fresh_ranges, ingredient):
             count += 1
     return count
 
-fresh_list = fresh_list(fresh_ranges)
-print(count_fresh_ingredients(ingredients, fresh_list))
+
+input = open('inputs/input.txt', 'r').read()
+fresh_ranges, ingredients = format_input(input)
+
+print(count_fresh_ingredients(ingredients, fresh_ranges))
