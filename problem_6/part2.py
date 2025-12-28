abd1b6435e8d609cff2c6859d7b0f9b4984169ac
Victeldo <44360797+Victeldo@users.noreply.github.com>
2025-12-27 23:27:50 -0800

    part 2 second round of issues but adding this commit for documentation's sake

--- problem_6/part2.py
+++ problem_6/part2.py
@@ -29,10 +29,6 @@ def cephalopod_total(grid, operators):
             final_score += (multiply_problems(grid, index))
     return final_score
 
-input = open('inputs/input1.txt', 'r').read()
-input = input.split('\n')
-grid, operators = format_grid(input)
-
 # print(cephalopod_total(grid, operators))
 
 
@@ -46,7 +42,8 @@ top to bottom, we compose numbers by column. adding trailing 0s helps a lot.
 so we get 4, 431, 623
 
 so given some column of numbers in strings, we must do the following:
-1. find out the longest number in the column, and add trailing 0s to make shorter numbers equivalent in length.
+1. find out the longest number in the column, and add trailing #s to make shorter numbers equivalent in length.
+1.5 trailing #s is insufficient, we need to make sure to add to correct side, not just blindly to the end
 2. string concatenate numbers top to bottom by index. row_0 + row_1 + row_2 + ...
 3. convert the concatenated string into an integer and add to a list.
 4. repeat this for all columns to get all the numbers.
@@ -59,21 +56,37 @@ def format_numbers(grid, index):
     ### raw numbers
     for row in grid:
         numbers.append(row[index])
-        print(numbers)
     
-    ## find longest and add trailing 0s
+    # find longest and add trailing #s
     longest = max(numbers, key=len)
-    for number in numbers:
+    for idx, number in enumerate(numbers):
         if len(number) < len(longest):
-            number += '0' * (len(longest) - len(number))
+            numbers[idx] += '#' * (len(longest) - len(number))
+    
+    ## string concatenate each number top to bottom by string index, skipping the #s
 
-    ## string concatenate each number top to bottom by string index (now that all of equal length). row_0 + row_1 + row_2 + ...
+    cleaned_numbers = []
+
     for i in range(len(numbers[0])):
         concatenated = ''
-        for number in numbers:
-            concatenated += number[i]
-        numbers.append(concatenated)
+        for idx, number in enumerate(numbers):
+            if number[i] != '#':
+                concatenated += numbers[idx][i]
+        cleaned_numbers.append(int(concatenated))
     
-    return numbers
+    print(cleaned_numbers)
 
+    # ## string concatenate each number top to bottom by string index (now that all of equal length). row_0 + row_1 + row_2 + ...
+    # for i in range(len(numbers[0])):
+    #     concatenated = ''
+    #     for number in numbers:
+    #         concatenated += number[i]
+    #     numbers.append(concatenated)
+    
+    # return numbers
+
+
+input = open('inputs/input1.txt', 'r').read()
+input = input.split('\n')
+grid, operators = format_grid(input)
 print(format_numbers(grid, 0))
