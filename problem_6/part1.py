60244a4ad1366824875825dc91d886bd92e3710f
Victeldo <44360797+Victeldo@users.noreply.github.com>
2025-12-27 22:47:26 -0800

    cleaned up part 1

--- problem_6/part1.py
+++ problem_6/part1.py
@@ -1,19 +1,10 @@
-### input is vertically stacked, need to make a grid, i.e. each row is an array (and each vertical column/parallel index is a problem)
-### need to add all the vertical problems together to get the total score. Each row is separated by spaces
-import math
-
-input = open('inputs/input.txt', 'r').read()
-
-input = input.split('\n')
-
-grid = []
-
-for line in input:
-    values = line.split()
-    grid.append(values)
-
-operators = grid.pop() ## such that the grid only contains the numbers
-### length of grid is the number of args + operator for a problem. length of any row is the number of problems.
+def format_grid(input):
+    grid = []
+    for line in input:
+        values = line.split()
+        grid.append(values)
+    operators = grid.pop()
+    return grid, operators
 
 def add_problems(grid, index):
     total_score = 0
@@ -29,11 +20,17 @@ def multiply_problems(grid, index):
     
     return total_score
 
-final_score = 0
-for index, operator in enumerate(operators):
-    if operator == '+':
-        final_score += (add_problems(grid, index))
-    elif operator == '*':
-        final_score += (multiply_problems(grid, index))
+def cephalopod_total(grid, operators):
+    final_score = 0
+    for index, operator in enumerate(operators):
+        if operator == '+':
+            final_score += (add_problems(grid, index))
+        elif operator == '*':
+            final_score += (multiply_problems(grid, index))
+    return final_score
 
-print(final_score)
+input = open('inputs/input.txt', 'r').read()
+input = input.split('\n')
+grid, operators = format_grid(input)
+
+print(cephalopod_total(grid, operators))
