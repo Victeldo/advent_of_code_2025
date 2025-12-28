b1b6a6ecd3bc966fefb8e5950a610ddae6679837
Victeldo <44360797+Victeldo@users.noreply.github.com>
2025-12-28 08:54:42 -0800

    cleaned up part 1 day 7

--- problem_7/part1.py
+++ problem_7/part1.py
@@ -1,33 +1,32 @@
-## downward count the indices we have beams.
-## we need to count how many times a split is rendered, so basically when we update the downward beam count, we also update a counter for the split.
 
-"""
-pseudocode:
-lasers = [] ## start with index of S
-go down a row, if row[x] == lasers[x] update new list to have left and right indices, and not original and add to the split count.
-if no split, just retain the original index.
-"""
+def starting_lasers(input):
+    lasers = []
+    for idx in range(len(input[0])):
+        if input[0][idx] == 'S':
+            lasers.append(idx)
+    return lasers
 
-input = open('inputs/input.txt', 'r').read()
-input = input.split('\n')
-
-lasers = []
-for idx in range(len(input[0])):
-    if input[0][idx] == 'S':
-        lasers.append(idx)
-
-
-count = 0
-for round in range(1, len(input)):
+def update_lasers(row, lasers, count):
     new_lasers = []
-    for idx in range(len(input[round])):
+    for idx in range(len(row)):
         if idx in lasers:
-            if input[round][idx] == '^':
+            if row[idx] == '^':
                 new_lasers.append(idx - 1)
                 new_lasers.append(idx + 1)
                 count += 1
             else:
                 new_lasers.append(idx)
-    lasers = new_lasers
+    return new_lasers, count
 
-print(count)
+def go_to_the_bottom(input, lasers):
+    count = 0
+    for row in range(1, len(input)):
+        new_lasers, count = update_lasers(input[row], lasers, count)
+        lasers = new_lasers
+    return count
+
+input = open('inputs/input.txt', 'r').read()
+input = input.split('\n')
+
+lasers = starting_lasers(input)
+print(go_to_the_bottom(input, lasers))
